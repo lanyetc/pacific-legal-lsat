83a0a5f121fc4dd42d04cee32e7f3f0a40e9c1d6
annnnnieee <anastasiya.vt.s@gmail.com>
2020-02-06 20:11:21 -0800

    refactored chat component

--- src/containers/Assessment/Chatbot/Chat.tsx
+++ src/containers/Assessment/Chatbot/Chat.tsx
@@ -4,83 +4,27 @@ import ChatbotMessage from './ChatbotMessage'
 import UserMessage from "./UserMessage";
 
 
-export default class Chat extends React.Component {
-    
-    constructor(props: any) {
-        super(props);
-    }
+export default function Chat(props: any) { 
+  const messageOptions =  props.data.messages.map((message: any) => {
+      
+      if(message.content){  
+          return (
+          <div>
+              <ChatbotMessage content={message.content}></ChatbotMessage>
+              {message.options && <UserMessage userOptions={message.options}></UserMessage>}
+          </div>
+          )
+      } 
+  });
 
-    // garbage data - will need to be passed through props
-    private data = {
-        messages: [ // this will need to be encoded in classes. 
-          {
-            id: 1,
-            type: "multi",
-            content: 'Intro',
-            options: [
-              {
-                id: 700,
-                label: "option 1",
-              },
-              {
-                id: 701,
-                label: "option 2",
-              }
-            ],
-            extraInfo: [],
-            triggers: [
-                {
-                    answers: [
-                        {
-                            questionId: 1,
-                            optionId: 700
-                        }
-                    ],
-                    results: [
-                        "some result for q 1"
-                    ],
-                    response: ["some response"],
-                    nextQuestionId: 1
-                }
-            ]
-          },
-          {
-            id: 2,
-            content: 'some contnent',
-            options: [
-              {
-                id: 888,
-                label: "some label"
-              }
-            ],
-            extraInfo: [],
-            triggers: []
-          }
-        ],
-      }
+  return(
+  <div className="content-container">
+      <div className="chat-area">
+          {messageOptions}   
+      </div>
+  </div>
+  )
 
-
-    render(){
-       const messageOptions =  this.data.messages.map((message) => {
-           
-            if(message.content){
-               return (
-                <div>
-                    <ChatbotMessage content={message.content}></ChatbotMessage>
-                    {message.options && <UserMessage userOptions={message.options}></UserMessage>}
-               </div>
-               )
-            } 
-        });
-
-        return(
-        <div className="content-container">
-            <div className="chat-area">
-                {messageOptions}   
-            </div>
-        </div>
-        )
-    }
 
 }
 
--- src/containers/Assessment/Chatbot/ChatbotPage.tsx
+++ src/containers/Assessment/Chatbot/ChatbotPage.tsx
@@ -5,31 +5,85 @@ import Header from '../../../components/Header/Header'
 import HeaderLinks from "../../../components/Header/HeaderLink"
 
 import ProgressBar from "./ProgressBar";
-import ChatContentSection from "./Chat";
+import Chat from "./Chat";
 import ToDoSection from "./ToDoSection";
 import {getSurvey} from "../../../data/data";
-const ChatbotPage = () => {
-  let survey = getSurvey();
-  console.log(survey);
-  return (
-    <div className="full-screen-container grey">
-      <Header
-                brand="PLEO"
-                toolTitle = "NPO SELF ASSESSMENT "
-                fixed
-                color="white"
-                rightLinks={<HeaderLinks />}
-                absolute
-               
-        />
-         <div className="main-container">
-            <ProgressBar ></ProgressBar>
-            <ChatContentSection></ChatContentSection>
-            <ToDoSection></ToDoSection>
-        </div>
+export default class ChatbotPage extends React.Component {
+  private survey = getSurvey();
+  private data = {
+    messages: [ // this will need to be encoded in classes. 
+      {
+        id: 1,
+        type: "multi",
+        content: 'Intro',
+        options: [
+          {
+            id: 700,
+            label: "option 1",
+          },
+          {
+            id: 701,
+            label: "option 2",
+          }
+        ],
+        extraInfo: [],
+        triggers: [
+            {
+                answers: [
+                    {
+                        questionId: 1,
+                        optionId: 700
+                    }
+                ],
+                results: [
+                    "some result for q 1"
+                ],
+                response: ["some response"],
+                nextQuestionId: 1
+            }
+        ]
+      },
+      {
+        id: 2,
+        content: 'some contnent',
+        options: [
+          {
+            id: 888,
+            label: "some label"
+          }
+        ],
+        extraInfo: [],
+        triggers: []
+      }
+    ],
+  }
 
-    </div>
-  );
+  constructor(props: any) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render(){
+    return (
+      <div className="full-screen-container grey">
+        <Header
+                  brand="PLEO"
+                  toolTitle = "NPO SELF ASSESSMENT "
+                  fixed
+                  color="white"
+                  rightLinks={<HeaderLinks />}
+                  absolute
+                 
+          />
+           <div className="main-container">
+              <ProgressBar ></ProgressBar>
+              <Chat data={this.data}></Chat>
+              <ToDoSection></ToDoSection>
+          </div>
+  
+      </div>
+    );
+  }
+  
 }
 
-export default ChatbotPage;
