5b5352941c24908a8d7ce5a9a55ce8d4656d07e3
YB <bieyi1224@gmail.com>
2020-02-10 15:19:10 -0800

    no message

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './FooterStyle.css';
 
-export default function Footer(props: any) { 
-  return(
-  <div className="">
-      
-  </div>
+export default function Footer(props: any) {
+  return (
+      <footer className="footerResults">
+        <h1>Sign Up For This</h1>
+        <section className="footerInnerResults">
+
+        </section>
+      </footer>
   )
 
 
