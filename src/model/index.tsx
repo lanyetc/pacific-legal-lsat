57f7041b693de94e2cbb8071cd3d36799ba91309
Annnnnieee <asokha@paypal.com>
2020-03-05 15:02:35 -0800

    fixed option matcher export

--- src/model/index.tsx
+++ src/model/index.tsx
@@ -3,5 +3,5 @@ export {Trigger} from './Trigger'
 export {SingleSelectQuestion} from './SingleSelectQuestion'
 export {MultiSelectQuestion} from './MultiSelectQuestion'
 export {Node, NodeFactory, NodeType} from './Node' // export NodeData when after deleting the data.tsx. cant rexport it right now.
-export {OptionMatcher, MatchAllOptions, MatchSomeOptions} from './OptionMatcher'
+export * from './OptionMatcher'
 export {AutoPlayMessage} from './AutoPlayMessage'
