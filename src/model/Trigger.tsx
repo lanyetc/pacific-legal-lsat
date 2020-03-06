0f6cbc2e49b722125874d51aa5a2a7e1ce6b9d96
lanyetc <2216446158@qq.com>
2020-03-05 16:08:31 -0800

    define trigger class

--- src/model/Trigger.tsx
+++ src/model/Trigger.tsx
@@ -1,12 +1,24 @@
 import {Condition} from './index'
 export class Trigger{ 
     constructor(
-        private _condition: Condition[],
-        private _action: any){
+        private _condition: Condition,
+        private _action: any,
+        private _todo?: any,
+        private _reminder?: any){
 
         }
 }
 
-export class TriggerFactory{
+export interface TriggerData {
+    condition: Condition,
+    action: any,
+    todo?: any,
+    reminder?: any
+}
 
+export class TriggerFactory{
+    creatTriggerFromData(tigger: TriggerData){
+        let { condition, action, todo, reminder } = tigger
+        return new Trigger(condition, action, todo, reminder);
+    }
 }
