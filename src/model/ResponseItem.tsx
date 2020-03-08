b6115dec408cc0c65f82557c9a4ff0e3d036ea71
lanyetc <2216446158@qq.com>
2020-03-07 16:55:16 -0800

    implement responseItem & responsePath add test for them

--- src/model/ResponseItem.tsx
+++ src/model/ResponseItem.tsx
@@ -3,7 +3,21 @@ export class ResponseItem {
     constructor(private _messageId: number, 
                 private _optionIds: number[]){ // store all selected option ids of a message
                 }
+    // getters/setters
+    get messageId() {
+        return this._messageId;
+    }
+    get optionIds() {
+        return this._optionIds;
+    }
+    public findResponseOption(optionId: number):boolean {
+        if (this.optionIds.indexOf(optionId) >= 0) {
+            return true; // find matched optionid
+        }
+        return false;
+    }
 }
+
 
 // Question: do we need ResponseFactory?
 // ResponseItem is now only generated in chatbot page
--- src/model/ResponsePath.tsx
+++ src/model/ResponsePath.tsx
@@ -9,7 +9,22 @@ export class ResponsePath {
         this._responseMap = {}
     }
 
+    // getters/setters
+    get responseList() {
+        return this._responseList;
+    }
+    get responseMap() {
+        return this._responseMap;
+    }
+
     public findMessageResponse(messageId: number, optionId: number) {
         // TODO: implement this
+        let isFind:boolean = this.responseMap[messageId] && this.responseMap[messageId].findResponseOption(optionId) ? true : false;
+        return isFind;
+    }
+
+    public addResponseItem(responseItem: ResponseItem) {
+        this.responseList.push(responseItem);
+        this.responseMap[responseItem.messageId] = responseItem;
     }
 }
