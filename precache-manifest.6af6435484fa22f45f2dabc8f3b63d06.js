f745f19fefb7f23e1fd09d99736f0d94639a5f63
lanyetc <2216446158@qq.com>
2020-03-09 17:17:20 -0700

    Updates

--- precache-manifest.6af6435484fa22f45f2dabc8f3b63d06.js
+++ precache-manifest.6af6435484fa22f45f2dabc8f3b63d06.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8f9798aa74444d5e61db0ed22b635d0",
+    "revision": "a9706ab131a4a3d7926d5469593fe3a5",
     "url": "/pacific-legal-lsat/index.html"
   },
   {
-    "revision": "d0b6d7e6a77b856f8162",
-    "url": "/pacific-legal-lsat/static/css/main.dbdaa0a1.chunk.css"
+    "revision": "7892c8ded05c3ea6dd6f",
+    "url": "/pacific-legal-lsat/static/css/main.be64fc3c.chunk.css"
   },
   {
     "revision": "1067d7d82a856b09a0bd",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pacific-legal-lsat/static/js/2.778ec86f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d0b6d7e6a77b856f8162",
-    "url": "/pacific-legal-lsat/static/js/main.47d73d5f.chunk.js"
+    "revision": "7892c8ded05c3ea6dd6f",
+    "url": "/pacific-legal-lsat/static/js/main.9b1eb08a.chunk.js"
   },
   {
     "revision": "f26db3a15b7c96bc593d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pacific-legal-lsat/precache-manifest.c03a3212365e255ec31e94ef3e50ed09.js"
+  "/pacific-legal-lsat/precache-manifest.6af6435484fa22f45f2dabc8f3b63d06.js"
 );
 
 self.addEventListener('message', (event) => {
