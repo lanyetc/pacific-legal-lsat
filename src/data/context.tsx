109a0bc7d6f63c413d82b49b679ede6ea7c4abbe
annnnnieee <anastasiya.vt.s@gmail.com>
2020-03-17 17:32:26 -0700

    cleaned up context

--- src/data/context.tsx
+++ src/data/context.tsx
@@ -29,54 +29,6 @@ export interface Context {
     reminders: Array<Item>;
 }
 
-// Context data generating functions. Only for testing
-// function getPath() {
-//     let pathList: Array<ResponseItem> = [];
-//     // pathList.push({ _messageId: 1, optionIds: [100] });
-//     // pathList.push({ _messageId: 3, optionIds: [300] });
-//     // pathList.push({ _messageId: 4, optionIds: [401] });
-//     return pathList;
-// }
-
-// function getTodoList() {
-//     let todoList: Array<Item> = [];
-//     todoList.push({ title: "Create a privacy policy" });
-//     todoList.push({ title: "Review social media terms of service" });
-//     todoList.push({ title: "Outline personal info access " });
-//     todoList.push({ title: "Train employees on privacy policies" });
-//     return todoList;
-// }
-
-// function getReminders() {
-//     let reminderList: Array<Item> = [];
-//     reminderList.push({ title: "Only use mass messages for fundraising" });
-//     reminderList.push({ title: "Get consent before sending mass messages" });
-//     reminderList.push({ title: "Lorem ipsum dolor sit amet" });
-//     return reminderList;
-// }
-
-// function getResultList() {
-//     let resultList: Array<Result> = [];
-//     resultList.push({ questionId: 1, optionId: 100, repo: "result repo no 1" });
-//     resultList.push({ questionId: 3, optionId: 300, repo: "result repo no 3" });
-//     resultList.push({ questionId: 4, optionId: 401, repo: "result repo no 4" });
-//     return resultList;
-// }
-
-// export function getContext() {
-//     let path = getPath();
-//     let todos = getTodoList();
-//     let reminders = getReminders();
-//     let results = getResultList();
-//     let module = { name: "Privacy Policy", path: path, todos: todos, reminders: reminders, results: results };
-//     let context: Context = {
-//         moduleResults: { 1: module },
-//         todos: [],
-//         reminders: []
-//     };
-//     return context;
-// }
-
 // default context
 const defaultContext: Context = {
     moduleResults: {},
@@ -134,8 +86,3 @@ export class ResultContextProvider extends React.Component {
 }
 
 export const ResultContextConsumer = ResultContext.Consumer
-
-// export const ResultContext = React.createContext(
-//     context,
-//     updateContext: (id: number, contextItm: any) => {}
-// )
