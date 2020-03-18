ba62b7fc428fc25403c3a1f9afb3f4d160b2a204
lanyetc <2216446158@qq.com>
2020-03-18 16:16:57 -0700

    merge

--- src/containers/LandingPage/LandTopAnim.tsx
+++ src/containers/LandingPage/LandTopAnim.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-import {Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import TopAnim from '../../Assets/img/animation-final.gif';
 
 import './LandingPageStyle.css';
 import { withRouter } from "react-router-dom";
 
-let LandTopAnim: any = function(props: any) {
-    let {history} = props
+let LandTopAnim: any = function (props: any) {
+    let { history } = props
     return (
         <div className="LandTopwithPic">
             <div className="landleftwords">
-                <span className="h1title">LSALT 2.0</span>
+                <span className="h1title">LSALT2.0</span>
                 <span className="h2title">Legal compliance simplified</span>
                 <div className="topwords">We understand that non-profits are driven by passion, and we want to help you focus on your contributions to society and making this world a better place.
 </div>
