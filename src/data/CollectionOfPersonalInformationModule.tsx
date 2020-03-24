7143d4e2e9fea25b6076a820664feb1e346a5890
lanyetc <2216446158@qq.com>
2020-03-24 10:14:26 -0700

    fix text

--- src/data/CollectionOfPersonalInformationModule.tsx
+++ src/data/CollectionOfPersonalInformationModule.tsx
@@ -132,7 +132,7 @@ export let collectionOfPersonalInformationModule:any = [
       {
         id: 7,
         type: MessageType.autoPlayMessage,
-        content: "If your purpose for collecting information is obvious and voluntarily, like collecting an email address when someone signs up for your newsletter, this is implied consent. \n\n If someone explicitly agrees to the collection, use, and disclosure of their information (by signing a consent form, for example) this is explicit consent. ",
+        content: "If your purpose for collecting information is obvious and voluntary, like collecting an email address when someone signs up for your newsletter, this is implied consent. \n\n If someone explicitly agrees to the collection, use, and disclosure of their information (by signing a consent form, for example) this is explicit consent. ",
         options: [],
         triggers: [
           {
